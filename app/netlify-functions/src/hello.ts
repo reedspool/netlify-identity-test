218179d3c049dda17cd0adb6ef74df5006c60a94
Reed Spool <reed@heysparkbox.com>
2022-08-02 00:38:21 -0600

    First commit: Working static page and hello world function

---
+++ app/netlify-functions/src/hello.ts
@@ -0,0 +1,40 @@
+import { Handler } from "@netlify/functions";
+
+export const basicHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, OPTIONS",
+}
+
+export const htmxHeaders = {
+    ...basicHeaders,
+    "Access-Control-Allow-Headers": [
+        "Content-Type",
+        "HX-Current-URL",
+        "HX-Request",
+        "HX-Boosted",
+        "HX-Current-URL",
+        "HX-History-Restore-Request",
+        "HX-Prompt",
+        "HX-Request",
+        "HX-Target",
+        "HX-Trigger-Name",
+        "HX-Trigger"
+    ].join(", "),
+    "Access-Control-Expose-Headers": [
+        "HX-Push",
+        "HX-Redirect",
+        "HX-Refresh",
+        "HX-Retarget",
+        "HX-Trigger",
+        "HX-Trigger-After-Settle",
+        "HX-Trigger-After-Swap"
+    ].join(", "),
+};
+
+export const handler: Handler = async (event, context) => {
+    return {
+        statusCode: 200,
+        headers: htmxHeaders,
+        body: JSON.stringify({ message: "Hello World" }),
+    };
+};
