e5c5e8f317399c41d1fd542b728cfcbb78ca4a56
Reed Spool <reed@heysparkbox.com>
2022-08-03 16:49:42 -0600

    feature: require auth and add identity to user entry

--- app/netlify-functions/src/setName.ts
+++ app/netlify-functions/src/setName.ts
@@ -2,10 +2,25 @@ import { Handler } from "@netlify/functions";
 import { parseJsonBody, htmxHeaders, assertSchema } from "lib/netlify-function-utilities/src"
 import { client as faunaClient, timestampNow, q } from "lib/fauna/src";
 
-export const handler: Handler = async (event) => {
+export const handler: Handler = async (event, context) => {
+    if (!context.clientContext?.user) {
+        console.warn(`Unauthenticated request to '${event.rawUrl}'`)
+
+        return {
+            statusCode: 401,
+            headers: htmxHeaders,
+            body: JSON.stringify({ message: 'Authentication required' }),
+        };
+    }
+
     const data = parseJsonBody(event);
 
     assertSchema(data, { name: "string" });
+
+    const identity = context.clientContext?.user?.sub;
+
+    if (typeof identity !== "string")
+        throw new Error(`Case of mistaken identity: '${identity}'`)
 
     // assertSchema does not ensure there are no extra properties (it could), so
     // we must unpack and repack to save, lest we allow bad actors to push
@@ -15,7 +30,8 @@ export const handler: Handler = async (event) => {
     try {
         const expr = q.Create(q.Collection("user"), {
             data: {
-                ...{ name },
+                name,
+                identity,
                 created_ts: timestampNow()
             }
         });
